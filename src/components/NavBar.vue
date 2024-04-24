701cfda3480ac47f3686071bf8bcf26e4b610d9c
Sandor SC <sandorsc941018@gmail.com>
2024-04-24 10:35:13 -0400

    close dropdowns on mobile

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,15 +2,16 @@
   <div class="navbar bg-white shadow rounded-lg">
     <div class="navbar-start">
       <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost p-1 lg:hidden">
+        <div @click="dropdownVisible = true" tabindex="0" role="button" class="btn btn-ghost p-1 lg:hidden">
           <Bars3CenterLeftIcon class="w-7 h-7" />
-        </label>
+        </div>
         <ul
+          v-show="dropdownVisible"
           tabindex="0"
           class="menu menu-compact dropdown-content mt-3 p-2 shadow-lg bg-white rounded-box w-40 md:w-52"
         >
-          <li><RouterLink to="/">Inicio</RouterLink></li>
-          <li><RouterLink to="/productos">Productos</RouterLink></li>
+          <li @click="dropdownVisible = false"><RouterLink to="/">Inicio</RouterLink></li>
+          <li @click="dropdownVisible = false"><RouterLink to="/productos">Productos</RouterLink></li>
           <li>
             <label for="contact-modal">Contacto</label>
           </li>
@@ -39,7 +40,7 @@
     </div>
     <div class="navbar-end">
       <div class="dropdown dropdown-end">
-        <label tabindex="0" class="btn btn-ghost btn-circle">
+        <label @click="dropdownCartVisible = true" tabindex="0" class="btn btn-ghost btn-circle">
           <div class="indicator">
             <ShoppingCartIcon class="w-6 h-6" />
             <span v-if="store.counter > 0" class="badge badge-sm bg-accent text-white border-none indicator-item">
@@ -48,6 +49,7 @@
           </div>
         </label>
         <div
+          v-show="dropdownCartVisible"
           tabindex="0"
           class="mt-3 card card-compact dropdown-content w-60 bg-white shadow"
         >
@@ -62,10 +64,10 @@
             </span>
             <span class="text-slate-500">Subtotal: ${{ store.mount.toFixed(2) }}</span>
             <div class="card-actions">
-              <label for="cart-modal" :class="[
-                  store.counter === 0 ? 'disabled' : '',
-                  'btn btn-outline btn-accent btn-block',
-                ]"
+              <label 
+                @click="dropdownCartVisible = false"
+                for="cart-modal" 
+                :class="[store.counter === 0 ? 'disabled' : '','btn btn-outline btn-accent btn-block',]"
               >
                 Ver Carrito
               </label>
@@ -94,7 +96,8 @@ import ContactModal from "../components/ContactModal.vue"
 
 const store = useCartStore()
 const link_color = "#003399"
-const showContactModal = ref(false)
+const dropdownVisible = ref(true)
+const dropdownCartVisible = ref(true)
 </script>
 
 <style lang="css">
